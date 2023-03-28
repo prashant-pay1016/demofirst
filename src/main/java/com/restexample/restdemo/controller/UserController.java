7b36d9d84802dce6e90dde5a837d014ff0469e7d
prashant-pay1016 <prashant@paynet.pro>
2023-03-29 02:15:21 +0530

    second commit

--- src/main/java/com/restexample/restdemo/controller/UserController.java
+++ src/main/java/com/restexample/restdemo/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.restexample.restdemo.controller;
 
 import com.restexample.restdemo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,7 +18,8 @@ import java.util.List;
 public class UserController {
 
     List<User> allUser = new ArrayList<>();
-    private User user;
+
+
 
     @RequestMapping(value = "/create-user", method = RequestMethod.POST)
     public ResponseEntity<?> createUser(@RequestBody User user) {
@@ -27,36 +33,39 @@ public class UserController {
 //    }
     @RequestMapping(value = "/fetch-user", method = RequestMethod.GET)
     public ResponseEntity<?> fetchAllUser() {
-        if (allUser != null) {
+        Integer customHttpStatusValue = 406;
+        if (!allUser.isEmpty()) {
             return ResponseEntity.ok(allUser);
         } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("no user found!");
+            return ResponseEntity.status(customHttpStatusValue).body("No user found! First insert an user");
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"No user found! First insert an user");
         }
     }
 
-    @RequestMapping(value = "/fetchid/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/fetch-id/{id}", method = RequestMethod.GET)
     public ResponseEntity<?> fetchById(@PathVariable("id") Integer id) {
         User obj = allUser.stream().filter(user -> id.equals(user.getId())).findFirst().orElse(null);
-
+          Integer customHttpStatusValue = 499;
         if (obj != null) {
             return ResponseEntity.ok(obj);
         } else {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("user not found!");
+            return ResponseEntity.status(customHttpStatusValue).body("No user found by this id: "+id);
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"User doesn't exist in table!");
         }
     }
 
 
-    @RequestMapping(value = "/deleteuser/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<?> deleteUserById(@PathVariable("id") Integer id) {
+    @RequestMapping(value = "/delete-user/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<String> deleteUserById(@PathVariable("id") Integer id) {
         boolean removed = allUser.removeIf(user -> user.getId() == id);
         if (removed) {
             return ResponseEntity.ok("Customer with ID " + id + " removed.");
         } else {
-            return ResponseEntity.notFound().build();
+            throw  new ResponseStatusException(HttpStatus.NOT_FOUND, "user not found!");
         }
     }
 
-    @RequestMapping(value = "/updateuser/{id}", method = RequestMethod.PUT)
+    @RequestMapping(value = "/update-user/{id}", method = RequestMethod.PUT)
     public ResponseEntity<?> updateById(@PathVariable("id") Integer id, @RequestBody User user) {
 
         User user1 = allUser.stream().filter(u -> u.getId().equals(id)).findFirst().orElse(null);
@@ -69,9 +78,18 @@ public class UserController {
             return ResponseEntity.ok(user1);
         } else {
 
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("user not found!");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"user not found! Please check the id again");
         }
 
+
+    }
+
+
+    @RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
+    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+         String fileName =  file.getOriginalFilename();
+         file.transferTo(new File("/home/parshant/Desktop/system_Image/" + file.getOriginalFilename()));
+         return ResponseEntity.ok(fileName);
 
     }
 
